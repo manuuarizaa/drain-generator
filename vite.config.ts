d70cf6d380c83fda3dd8f60afbddf0f717184e7a
Manu Ariza <manuariza95@gmail.com>
2026-06-18 19:48:18 +0200

    feat: add workflow for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  base: '/drain-generator/',
   plugins: [react()],
   build: {
     target: 'es2022',
